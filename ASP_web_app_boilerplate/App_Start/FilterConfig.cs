a209a4dfab0c38ce8bee77fde5c073e48d20f0d6
Mariem Ayadi <mayadi@smith.edu>
2018-02-11 12:10:32 -0500

    boiler

---
+++ ASP_web_app_boilerplate/App_Start/FilterConfig.cs
@@ -0,0 +1,10 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ASP_web_app_boilerplate {
+    public class FilterConfig {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters) {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ ASP_web_app_boilerplate/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ASP_web_app_boilerplate.MvcApplication" Language="C#" %>
